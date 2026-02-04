b9d41fa66952a866a2fe383b6ddfa821151f842b
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2026-02-04 10:50:38 +0530

    Values and variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Sri Lanka";
+let continent = "Asia";
+let population = "millions";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is Fun Bro!");
+console.log(25 + 10 + 5);
+console.log("Jonas");
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
